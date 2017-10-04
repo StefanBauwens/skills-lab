382b16c4601a312259195fafdb73a52472860e7a
cindyho97 <cindy.ho8197@gmail.com>
2017-10-04 09:31:11 +0200

    Replace test script

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
