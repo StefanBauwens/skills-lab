ec4ec6ece94b79d3e7d9eecb7f7620869cf1be5b
cindyho97 <cindy.ho8197@gmail.com>
2017-11-28 16:05:20 +0100

    Fixed tray colliding with layer

--- Assets/SkillsLab/Scripts/Tray.cs
+++ Assets/SkillsLab/Scripts/Tray.cs
@@ -8,16 +8,42 @@ public class Tray : MonoBehaviour {
 
     protected List<GameObject> children = new List<GameObject>();
     public Collider[] includeColliders;
+    public int detachWhenCollidingWithLayer;
+    public float breakTime;
+    protected float beginTime;
+    protected bool isColliding;
+    protected List<Collider> CollidingObjects;
 
     private void Start()
     {
         this.transform.parent.gameObject.GetComponent<Rigidbody>().centerOfMass = Vector3.zero;
+        isColliding = false;
+        CollidingObjects = new List<Collider>();
+    }
+
+    private void Update()
+    {
+        if (isColliding)
+        {
+            if (Time.time-beginTime > breakTime && this.transform.parent.parent != null)
+            {
+                this.transform.parent.parent = null; //get loose from controller
+                this.transform.parent.GetComponent<Rigidbody>().isKinematic = false;
+                isColliding = false;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (!includeColliders.Contains(other)) //if it's not a collider that is in the include list
         {
+            if (this.transform.parent.parent != null && detachWhenCollidingWithLayer == other.gameObject.layer) //if tray is grabbed
+            {
+                isColliding = true;
+                beginTime = Time.time;
+                CollidingObjects.Add(other);
+            }
             return;
         }
 
@@ -28,12 +54,21 @@ public class Tray : MonoBehaviour {
         }
     }
 
+
     private void OnTriggerExit(Collider other)
     {
         if (children.Contains(other.gameObject))
         {
             children.Remove(other.gameObject);
             other.gameObject.transform.parent = null; //changes the parent to none
+        }
+        else if (CollidingObjects.Contains(other))
+        {
+            CollidingObjects.Remove(other);
+            if (CollidingObjects.Count == 0)
+            {
+                isColliding = false;
+            }
         }
     }
 }
