da21a830bf491e3471e88dab1710ddb347e55dbb
cindyho97 <cindy.ho8197@gmail.com>
2017-12-02 14:31:34 +0100

    Lock end position drawers + fix BlueDrawer colliders

---
+++ Assets/SkillsLab/Scripts/LockDrawerEndPos.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockDrawerEndPos : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Drawer>())
+        {
+            other.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+}
