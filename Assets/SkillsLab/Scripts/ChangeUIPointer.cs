d2f4124902b446133c6e92d1a67dda1141f72d58
cindyho97 <cindy.ho8197@gmail.com>
2017-12-03 18:26:53 +0100

    Adjust Change UI Pointer script + keyboard higlight color and pinter size

--- Assets/SkillsLab/Scripts/ChangeUIPointer.cs
+++ Assets/SkillsLab/Scripts/ChangeUIPointer.cs
@@ -26,58 +26,25 @@ public class ChangeUIPointer : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name == "[VRTK][AUTOGEN][BodyColliderContainer]")
+        // If bodycollider collides with trigger collider
+        if (other.gameObject.name == "[VRTK][AUTOGEN][BodyColliderContainer]")
         {
-            Debug.Log("CameraRig!");
-            StartCoroutine(SetPointerRenderer(false));  
+            SetPointerRenderer(false);
         }
-        
     }
 
     private void OnTriggerExit(Collider other)
     {
+        // If bodycollider collides with trigger collider
         if (other.gameObject.name == "[VRTK][AUTOGEN][BodyColliderContainer]")
         {
-            Debug.Log("CameraRig!");
-            StartCoroutine(SetPointerRenderer(true));
+            SetPointerRenderer(true);
         }
     }
 
-    //private void SetPointerRenderer(bool setBezier)
-    //{
-    //    rightPointer.enabled = false;
-    //    leftPointer.enabled = false;
-    //    rightPointer.pointerRenderer.enabled = false;
-    //    leftPointer.pointerRenderer.enabled = false;
-
-    //    if (setBezier)
-    //    {
-    //        rightBezier.enabled = true; 
-    //        leftBezier.enabled = true;
-    //        rightStraight.enabled = false;
-    //        leftStraight.enabled = false;
-    //        rightPointer.pointerRenderer = rightBezier;
-    //        leftPointer.pointerRenderer = leftBezier;
-    //    }
-    //    else
-    //    {
-    //        rightBezier.enabled = false;
-    //        leftBezier.enabled = false;
-    //        rightStraight.enabled = true;
-    //        leftStraight.enabled = true;
-    //        rightPointer.pointerRenderer = rightStraight;
-    //        leftPointer.pointerRenderer = leftStraight;
-    //    }
-
-    //    rightPointer.pointerRenderer.enabled = true;
-    //    leftPointer.pointerRenderer.enabled = true;
-    //    rightPointer.enabled = true;
-    //    leftPointer.enabled = true;
-    //}
-
-    private IEnumerator SetPointerRenderer(bool setBezier)
+    // Set Bezier or Straight Point Renderer
+    private void SetPointerRenderer(bool setBezier)
     {
-        yield return new WaitForSeconds(1);
         rightPointer.enabled = false;
         leftPointer.enabled = false;
         rightPointer.pointerRenderer.enabled = false;
