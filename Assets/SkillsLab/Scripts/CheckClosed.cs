2b8ea13136d55c2597eca7f912f2afc7810e8065
Stefan Bauwens <stefan.bauwens@hotmail.com>
2017-12-06 11:32:52 +0100

    copy testcene for close detection vanas

---
+++ Assets/SkillsLab/Scripts/CheckClosed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckClosed : MonoBehaviour {
+    const float WAITTIMELOCK = 30f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+    }
+
+    public IEnumerator AutoLock(Drawer drawer)
+    {
+        yield return new WaitForSeconds(WAITTIMELOCK);
+
+    }
+}
