b6a85d85ff1b6c8e16d6896ef706e0a304ec276e
Stefan Bauwens <stefan.bauwens@hotmail.com>
2017-12-07 22:34:35 +0100

    Copy scene and make checkClosed script

--- Assets/SkillsLab/Scripts/CheckClosed.cs
+++ Assets/SkillsLab/Scripts/CheckClosed.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 
 public class CheckClosed : MonoBehaviour {
     const float WAITTIMELOCK = 30f;
+    protected List<Drawer> drawersToLock;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        drawersToLock = new List<Drawer>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.name);
+
+        if (other.gameObject.GetComponent<Drawer>())
+        {
+            if (other.gameObject.GetComponent<Drawer>().IsGrabbable && !drawersToLock.Contains(other.gameObject.GetComponent<Drawer>()))
+            {
+                drawersToLock.Add(gameObject.GetComponent<Drawer>());
+                StartCoroutine(AutoLock(other.gameObject.GetComponent<Drawer>()));
+            }
+        }
     }
 
     public IEnumerator AutoLock(Drawer drawer)
     {
         yield return new WaitForSeconds(WAITTIMELOCK);
-
+        drawer.SetGrabStatus(false);
+        drawer.SetRigidbodyStatus(false);
+        drawer.gameObject.GetComponent<CheckDrawerEmpty>().enabled = true;
+        drawersToLock.Remove(drawer);
     }
 }
