9ccb896115c540cf9797e6e9639f8459d6ce1408
Stefan Bauwens <stefan.bauwens@hotmail.com>
2017-12-09 22:03:12 +0100

    unlock doors after a while

--- Assets/SkillsLab/Scripts/LockDrawerEndPos.cs
+++ Assets/SkillsLab/Scripts/LockDrawerEndPos.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class LockDrawerEndPos : MonoBehaviour {
 
     public static bool isLocked;
+    protected const float SECONDSTOUNLOCK = 5f;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<Drawer>())
@@ -14,6 +16,16 @@ public class LockDrawerEndPos : MonoBehaviour {
             other.GetComponent<Drawer>().SetGrabStatus(false); // Can't grab so that objects colliders won't move
             other.GetComponent<Drawer>().SetRigidbodyStatus(false); // Freeze all axis 
             other.GetComponent<CheckDrawerEmpty>().enabled = true; // Enable script component  
+            StartCoroutine(Unlock(other.GetComponent<Drawer>()));
         }
+    }
+
+    IEnumerator Unlock(Drawer drawer)
+    {
+        yield return new WaitForSeconds(SECONDSTOUNLOCK);
+        isLocked = false;
+        drawer.SetGrabStatus(true);
+        drawer.SetRigidbodyStatus(true);
+        drawer.gameObject.GetComponent<CheckDrawerEmpty>().enabled = false;
     }
 }
