99f2e7cf63450b37da8d15abf571a31fd6c82c9a
Stefan Bauwens <stefan.bauwens@hotmail.com>
2017-12-18 21:45:56 +0100

    snap tubes to hand and other stuff

---
+++ Assets/SkillsLab/Scripts/DestroyMedicine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class DestroyMedicine : MonoBehaviour {
+
+    protected VRTK_SnapDropZone snapDropZone;
+
+    void Start()
+    {
+        snapDropZone = this.gameObject.GetComponent<VRTK_SnapDropZone>();
+
+        snapDropZone.ObjectSnappedToDropZone += new SnapDropZoneEventHandler(DestroyMed);
+    }
+
+	protected void DestroyMed(object sender, SnapDropZoneEventArgs e)
+	{
+        GameObject.Destroy ((GameObject)sender);
+	}
+}
---
+++ Assets/SkillsLab/Scripts/Sanitizer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sanitizer : MonoBehaviour {
+	const float ammountToMove = 0.009f;
+	const float factor = 0.01f;
+	protected Vector3 originalPosition;
+	static bool coroutineRunning;
+
+	// Use this for initialization
+	void Start () {
+		originalPosition = this.transform.position;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (!coroutineRunning) {
+			coroutineRunning = true;
+			StartCoroutine (Push ());
+		}
+	}
+
+	IEnumerator Push()
+	{
+		while (this.transform.position.y > originalPosition.y-ammountToMove) {
+			yield return new WaitForEndOfFrame ();
+			this.transform.position -= new Vector3 (0, factor*Time.fixedDeltaTime, 0);
+		}
+		yield return new WaitForSeconds (0.5f);
+		StartCoroutine (GoUp ());
+	}
+
+	IEnumerator GoUp()
+	{
+		while (this.transform.position.y < originalPosition.y) {
+			yield return new WaitForEndOfFrame ();
+			this.transform.position += new Vector3 (0, factor*Time.fixedDeltaTime, 0);
+			Debug.Log ("this rns");
+		}
+		coroutineRunning = false;
+	}
+}
