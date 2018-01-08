a404f2cc72f5c0fa140386821585d821ea060ab8
cindyho97 <cindy.ho8197@gmail.com>
2018-01-08 03:29:13 +0100

    Animate door open and close

---
+++ Assets/SkillsLab/Scripts/Door.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetBool("open", true);
+        Debug.Log("Open door");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("close", true);
+        Debug.Log("Close door");
+    }
+
+    public void ResetBoolean(string doorState)
+    {
+        if(doorState == "opened")
+        {
+            anim.SetBool("open", false);
+            Debug.Log("open bool reset");
+        }
+        else if(doorState == "closed")
+        {
+            anim.SetBool("close", false);
+            Debug.Log("close bool reset");
+        }   
+    }
+}
