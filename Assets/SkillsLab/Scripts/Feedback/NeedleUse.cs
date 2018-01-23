3a3149c96e935849eb5cafda962292c857564edc
Stefan Bauwens <stefan.bauwens@hotmail.com>
2018-01-23 11:50:23 +0100

    give needle options

---
+++ Assets/SkillsLab/Scripts/Feedback/NeedleUse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NeedleOption
+{
+    IV,
+    IM,
+    SC,
+    Transfer
+}
+public class NeedleUse : MonoBehaviour {
+    //used to check what kind of injections this needle can be used for
+    public NeedleOption[] CanBeUsedFor;
+}
